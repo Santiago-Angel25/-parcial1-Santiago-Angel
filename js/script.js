c4f35850c7236b470f98d7b7bb4d6d723d307b59
SantiagoAngel <santiago062225@gmail.com>
2026-03-10 07:59:45 -0500

    se agregro una caparpeta que contiene imagenes, se termino de modiciar el index y en el js se termino de relazar las validaciones de las exprecioens regualres

--- js/script.js
+++ js/script.js
@@ -17,21 +17,39 @@ function registrar() {
     let edad = document.getElementById("edad").value
     let tipoInteres = document.getElementById("Tipo").value
 
-    let regex = /@/
+    let regex = /^[a-zA-Z\s]+$/gi;
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let regexEdad = /^\d+$/;
+     
 
     if (nombre == "" || correo == "" || apellido == "" || ciudad == "" || edad == "" || tipoInteres == "Eleguir...") {
         alert("Todos los campos son obligatorios")
+        return
+    }
+     if(!nombre.match(regex) || !apellido.match(regex)){
+        alert("El nombre y apellido no pueden contener números o caracteres especiales")
+        return
+    }
+    if (!correo.match(regexEmail)) {
+        alert("Por favor, ingrese un correo electrónico válido")
         return
     }
     if(tipoInteres == "Eleguir..."){
         alert("Por favor, seleccione un tipo de servicio de interés")
         return
     }
-
-    if (regex.test = correo) {
-        alert("Correo válido")
+    if (!edad.match(regexEdad) || edad < 16 || edad > 120) {
+        alert("Por favor, ingrese una edad válida (entre 16 y 120 años)")
+        return
     }
+   
 
+    alert("Registro exitoso")
+    alert("Nombre: " + nombre + "\nApellido: " + apellido + "\nCorreo: " + correo + "\nCiudad: " + ciudad + "\nEdad: " + edad + "\nTipo de servicio de interés: " + tipoInteres)
+
+    document.getElementById("formRegistro").reset()
+
+   
 }
     let modoOscuro = false
 document.getElementById("modoBtn").addEventListener("click", function() {
